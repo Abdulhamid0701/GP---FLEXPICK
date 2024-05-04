88365c775ab09cade094da04ee7b7197b8564a91
Abdulhamid0701 <164815339+Abdulhamid0701@users.noreply.github.com>
2024-05-04 15:50:40 +0300

    belt interrupt speed

--- DeltaPositioner/src/Conveyor.h
+++ DeltaPositioner/src/Conveyor.h
@@ -1,8 +1,7 @@
 #include <math.h>
 
 // Belt's Stepper Motor Parameters 
-float belt_speed_rpm;
-float belt_speed_linear;
+float belt_speed_linear = 10; // mm/sec
 
 const float MicroSteps = 8;
 const float Step_Angle_Rads =  (2 * M_PI) / (200 * MicroSteps);
